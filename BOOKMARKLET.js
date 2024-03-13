3d1bc38b47956b1b9b0b50951bd5bbb0dc69ada5
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-13 13:38:26 -0500

    Create BOOKMARKLET.js

---
+++ BOOKMARKLET.js
@@ -0,0 +1,8 @@
+// NOTE: DO NOT USE THIS. USE "BOOKMARKLET.min.js" OR THE CODE SNIPPET IN "README.md".
+
+fetch("https://raw.githubusercontent.com/SY-Gato/KahootPls/main/GetKahootAnswers.js")
+.then((res) => {
+  res.text();
+}).then((t) => {
+  alert(res);
+});
