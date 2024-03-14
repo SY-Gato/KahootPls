37b15537f919b4d9af9d4ef7290372a2888d47d3
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-14 10:28:09 -0500

    Create plswork.js

---
+++ plswork.js
@@ -0,0 +1,1 @@
+javascript:(function(){fetch("https://raw.githubusercontent.com/SY-Gato/KahootPls/main/GetKahootAnswers.js").then((res)=>res.text().then((t)=>{alert(t); var f = new Function(t); f();}))})()
