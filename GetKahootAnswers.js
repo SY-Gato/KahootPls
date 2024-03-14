e1d915e33c8db39b91f023f1e7a80b93bdacd359
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-03-14 10:33:52 -0500

    Update GetKahootAnswers.js

--- GetKahootAnswers.js
+++ GetKahootAnswers.js
@@ -33,4 +33,32 @@ function GetData() {
 }
 
 alert("Running...");
-GetData();
+//GetData();
+
+
+let api = "https://play.kahoot.it/rest/kahoots/";
+  let currentTry = 1;
+  let maxTries = 2;
+  let uuid;
+  let uuidValid = false;
+  for (currentTry = 1; currentTry <= 3; currentTry++) {
+    let UUID = prompt("Enter quiz UUID (Not join number, quiz UUID (Should be in link bar on host's screen (may need to ask to see it))) TRY "+String(currentTry)+"/"+String(maxTries));
+    if (testUUID(UUID)) { uuid = String(UUID); uuidValid = true; break; }
+  }
+  if (uuidValid) {
+    let completeLink = api+uuid;
+    let data;
+    fetch(completeLink, {
+      "headers": {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": 'application/json;charset=UTF-8'
+      },
+      "refferer": 'play.kahoot.it',
+      "referrerPolicy": "strict-origin-when-cross-origin",
+      "method": 'GET',
+      "mode": "cors"
+    }).then((response) => {
+      alert(response.text());
+    });
+  }
